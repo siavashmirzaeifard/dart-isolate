1863b857dd7e7745be30d64c69d95fcf56d74fa1
Siavash Mirzaei-Fard <63229654+siavashmirzaeifard@users.noreply.github.com>
2022-04-27 17:38:03 +0430

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,18 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:isolate';
+import 'package:async/async.dart' show StreamGroup;
+
+import 'dart:developer' as devtools show log;
+
 import 'package:flutter/material.dart';
+
+extension Log on Object {
+  void log() => devtools.log(toString());
+}
+
+/* for this purpose I used "Live Server" extension for "VSCode" and instead of using live server load the names from the "api" folder which contains a json file as a hard coded json object. so press
+"cmd + shift + p" on mac or "ctrl + shift + p" on windows or linux and type live server and select on "open live server" and go to api/people.json, the url is your base url. (http://127.0.0.1:5500/api/people.json) */
 
 void main() {
   runApp(const MyApp());
@@ -6,110 +20,170 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+@immutable
+class Person {
+  final String name;
+  final int age;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  const Person({
+    required this.name,
+    required this.age,
+  });
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  Person.fromJson(Map<String, dynamic> json)
+      : name = json["name"] as String,
+        age = json["age"] as int;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  String toString() => "Person (name: $name, age: $age)";
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+/* our isolate main function, to send back the result object we pass the "SendPort" parameter here. sp.send() is our tunnel but it does not exit from the main function, so we pass the sp in Isolate.exit() and
+pass the sp and our result. */
+void _getPerson(SendPort sp) async {
+  const url = "http://127.0.0.1:5500/api/people1.json";
+  final persons = await HttpClient()
+      .getUrl(Uri.parse(url))
+      .then((req) => req.close())
+      .then((res) => res.transform(utf8.decoder).join())
+      .then((jsonString) => json.decode(jsonString) as List<dynamic>)
+      .then((json) => json.map((element) => Person.fromJson(element)));
+  Isolate.exit(
+    sp,
+    persons,
+  );
+}
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+/* our isolate enterance function, this function should setup our isolate and get the result and send the result back to it's caller. therefore here we have the ReceivePort and this receive port will use in
+the function and say hey I'm the tunnel and will give you the responses from send port and it will return's streams. Receive port is Read/Write, but sent port is Write-Only. for receiving data from send port we should call Isolate.spawn()
+method which gets from you a void function as first parameter(should be a void function), and for second parameter gives from you the message(data) which your first void function parameter accept's. */
+Future<Iterable<Person>> getPerson() async {
+  final rp = ReceivePort();
+  Isolate.spawn(
+    _getPerson,
+    rp.sendPort,
+  );
+  return await rp.first;
+}
+
+/* our isolate main function, to send back the result object we pass the "SendPort" parameter here. sp.send() is our tunnel but it does not exit from the main function, so we pass the sp in Isolate.exit() and
+pass the sp and our result. this Isolate.exit() works after 10 time that we defined and during this time sp.send(now) sending our date time to receive port. */
+void _getMessage(SendPort sp) async {
+  await for (final now in Stream.periodic(
+    const Duration(seconds: 1),
+    (_) => DateTime.now().toIso8601String(),
+  ).take(10)) {
+    sp.send(now);
   }
+  Isolate.exit(sp);
+}
 
+/* our isolate enterance function, this function should setup our isolate and get the result and send the result back to it's caller. therefore here we have the ReceivePort and this receive port will use in
+the function and say hey I'm the tunnel and will give you the responses from send port and it will return's streams. Receive port is Read/Write, but sent port is Write-Only. for receiving data from send port we should call Isolate.spawn()
+method which gets from you a void function as first parameter(should be a void function), and for second parameter gives from you the message(data) which your first void function parameter accept's.
+for this example because of the function returns string of date time now, the Isolate.exit(sp) returns null, and we check the receive port until getting null value. */
+Stream<String> getMessage() {
+  final rp = ReceivePort();
+  return Isolate.spawn(_getMessage, rp.sendPort)
+      .asStream()
+      .asyncExpand((_) => rp)
+      .takeWhile((element) => element is String)
+      .cast();
+}
+
+/* 3rd example */
+@immutable
+class Request {
+  final SendPort sp;
+  final Uri uri;
+  const Request(
+    this.sp,
+    this.uri,
+  );
+  Request.fromPersonsRequest(PersonsRequest request)
+      : sp = request.rp.sendPort,
+        uri = request.uri;
+}
+
+void _getPeople(Request request) async {
+  final persons = await HttpClient()
+      .getUrl(request.uri)
+      .then((req) => req.close())
+      .then((res) => res.transform(utf8.decoder).join())
+      .then((jsonString) => json.decode(jsonString) as List<dynamic>)
+      .then((json) => json.map((element) => Person.fromJson(element)));
+  // request.sp.send(persons);
+  Isolate.exit(
+    request.sp,
+    persons,
+  );
+}
+
+Stream<Iterable<Person>> getPeople() {
+  final streams = PersonsRequest.all().map((req) =>
+      Isolate.spawn(_getPeople, Request.fromPersonsRequest(req))
+          .asStream()
+          .asyncExpand((_) => req.rp)
+          .takeWhile((element) => element is Iterable<Person>)
+          .cast());
+
+  return StreamGroup.merge(streams).cast();
+}
+
+/* we will create 3 times this persons request url */
+@immutable
+class PersonsRequest {
+  final ReceivePort rp;
+  final Uri uri;
+  const PersonsRequest(
+    this.rp,
+    this.uri,
+  );
+  static Iterable<PersonsRequest> all() sync* {
+    for (final i in Iterable.generate(3, (i) => i)) {
+      yield PersonsRequest(
+        ReceivePort(),
+        Uri.parse("http://127.0.0.1:5500/api/people${i + 1}.json"),
+      );
+    }
+  }
+}
+
+void tester() async {
+  await for (final msg in getMessage()) {
+    msg.log();
+  }
+  await for (final msg in getPeople()) {
+    msg.log();
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Home Page'),
+        centerTitle: true,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: TextButton(
+        onPressed: () async {
+          final person = await getPerson();
+          person.log();
+          tester();
+        },
+        child: const Text("Press"),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
